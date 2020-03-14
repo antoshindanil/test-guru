e70840f9c1708cca283e1ace4417fcad45caa7a6
Daniil Antoshin <antoshindanil@ya.ru>
2020-03-14 22:37:18 +0200

    Add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_and_belongs_to_many :users
 
   def self.tests_by_category (category_title)
     Test.joins(:category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :tests
+
   def passed_tests (level)
     Test.where(level: level)
   end
