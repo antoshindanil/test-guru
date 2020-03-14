23f4e25f19a0380f007438863678e918b5b0452f
Daniil Antoshin <antoshindanil@ya.ru>
2020-03-14 22:17:20 +0200

    Resolve

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.tests_by_category (category_title)
+    Test.joins(:category)
+        .where("categories.title" => category_title)
+        .order(title: :desc)
+  end
 end
