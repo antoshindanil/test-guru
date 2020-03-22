7504c0ff923ae3bad09176b9af376aa418fe9a27
Danil Antoshin <antoshindanil@ya.ru>
2020-03-22 23:43:07 +0200

    Add validation and scopes into answer, category and test models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,14 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  scope :correct, -> { where(correct: true) }
+
+  validates :body, presence: true
+  validate :validate_count, on: :create
+
+  private
+
+  def validate_count
+    errors.add(:base, 'More than 4 answers') if question.answers.size >= 4
+  end
 end
