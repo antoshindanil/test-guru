334a12a8621fc5dd131dbbf8688b51f1d32ed9fc
Daniil Antoshin <antoshindanil@ya.ru>
2020-04-11 14:44:22 +0200

    add test mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable,
+    :confirmable
 
   has_many :author, class_name: "Test"
   has_many :test_passages
